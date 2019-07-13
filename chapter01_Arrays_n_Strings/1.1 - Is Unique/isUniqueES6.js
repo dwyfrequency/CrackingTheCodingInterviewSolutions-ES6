5078dcaf7e30bcafc9916bf3e4f8762c491eae69
dwyfrequency <jdwy215@gmail.com>
2019-07-13 13:06:19 -0400

    adding time and space complexity

---
+++ chapter01_Arrays_n_Strings/1.1 - Is Unique/isUniqueES6.js
@@ -0,0 +1,12 @@
+/*
+string has all unique characters without using additional data structures
+*/
+// Time O(n^2) | Space O(1)
+const isUnique = str => {
+  for (let i = 0; i < str.length - 1; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) return false;
+    }
+  }
+  return true;
+};
