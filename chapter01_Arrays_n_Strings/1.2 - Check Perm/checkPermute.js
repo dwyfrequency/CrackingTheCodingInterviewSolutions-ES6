dd27431c52348c72868a18e2287de7254691d910
dwyfrequency <jdwy215@gmail.com>
2019-07-13 22:34:13 -0400

    adding more notes

--- chapter01_Arrays_n_Strings/1.2 - Check Perm/checkPermute.js
+++ chapter01_Arrays_n_Strings/1.2 - Check Perm/checkPermute.js
@@ -1,3 +1,5 @@
+// Time O(n log(n)) | Space O(n)
+// n log(n) b/c of the sort -> merge sort/quick sort is average case n log(n)
 var checkPermuteEs5 = function(stringOne, stringTwo) {
   // if different lengths, return false
   if (stringOne.length !== stringTwo.length) {
