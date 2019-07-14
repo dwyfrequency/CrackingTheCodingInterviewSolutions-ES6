8cb7eb02a52b0675450b6a3b0515429b73d34c67
dwyfrequency <jdwy215@gmail.com>
2019-07-14 11:44:01 -0400

    adding test cases

--- chapter01_Arrays_n_Strings/1.5 - OneAway/oneAway.test.js
+++ chapter01_Arrays_n_Strings/1.5 - OneAway/oneAway.test.js
@@ -1,14 +1,30 @@
 const { oneAway } = require('./oneAwayES6');
 describe('oneAway tests', () => {
+  // testing deletion
   test('should return false', () => {
     expect(oneAway('jack', 'snack')).toBe(false);
   });
+  test('should return true', () => {
+    expect(oneAway('jack', 'jacki')).toBe(true);
+  });
+
+  // testing update
   test('should return false', () => {
     expect(oneAway('zach', 'jack')).toBe(false);
   });
   test('should return true', () => {
     expect(oneAway('zack', 'jack')).toBe(true);
   });
+
+  // testing insertion
+  test('should return false', () => {
+    expect(oneAway('ach', 'jack')).toBe(false);
+  });
+  test('should return true', () => {
+    expect(oneAway('zac', 'jack')).toBe(true);
+  });
+
+  // testing case insensitivity
   test('should be case insensitive', () => {
     expect(oneAway('ZaCk', 'Jack')).toBe(true);
   });
