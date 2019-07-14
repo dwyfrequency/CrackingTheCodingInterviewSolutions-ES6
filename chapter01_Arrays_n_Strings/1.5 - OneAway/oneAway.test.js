cb7f6cb9abc789bb65b7cd87cd48885039f35423
dwyfrequency <jdwy215@gmail.com>
2019-07-14 11:24:06 -0400

    adding tests for code

--- chapter01_Arrays_n_Strings/1.5 - OneAway/oneAway.test.js
+++ chapter01_Arrays_n_Strings/1.5 - OneAway/oneAway.test.js
@@ -1,1 +1,15 @@
-test('should ', () => {});
+const { oneAway } = require('./oneAwayES6');
+describe('oneAway tests', () => {
+  test('should return false', () => {
+    expect(oneAway('jack', 'snack')).toBe(false);
+  });
+  test('should return false', () => {
+    expect(oneAway('zach', 'jack')).toBe(false);
+  });
+  test('should return true', () => {
+    expect(oneAway('zack', 'jack')).toBe(true);
+  });
+  test('should be case insensitive', () => {
+    expect(oneAway('ZaCk', 'Jack')).toBe(true);
+  });
+});
---
+++ chapter01_Arrays_n_Strings/1.5 - OneAway/oneAwayES6.js
@@ -0,0 +1,6 @@
+// are we one edit/insert/delete away from the first string
+const oneAway = (str1, str2) => {};
+
+module.exports = {
+  oneAway,
+};
