59b5b7ae32be4e9ca848fceca891670152d0f9ba
dwyfrequency <jdwy215@gmail.com>
2019-07-18 17:59:57 -0400

    remove nth from the end

--- chapter02_LinkedLists/2.2 - Return Kth to Last/returnKthToLastES6.js
+++ chapter02_LinkedLists/2.2 - Return Kth to Last/returnKthToLastES6.js
@@ -1,6 +1,20 @@
 const returnKthToLast = (node, n) => {
   let leftMostNode = node;
   let currentNode = node;
+  // 2
+  // (a) -> (b) -> (c)
+  for (let i = 0; i < n; i++) {
+    currentNode = currentNode.next;
+  }
+  if (currentNode === null) {
+    // if it's null, then we return the head
+    return leftMostNode;
+  }
+  while (currentNode) {
+    currentNode = currentNode.next;
+    leftMostNode = leftMostNode.next;
+  }
+  return currentNode;
 };
 
 module.exports = returnKthToLast;
